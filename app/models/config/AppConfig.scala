877e766c63ec2360759a2284322f91792a5fe6e3
Cristian Schuszter <darkgeeky@gmail.com>
2016-09-04 23:00:06 +0300

    Added tiff to bp

--- app/models/config/AppConfig.scala
+++ app/models/config/AppConfig.scala
@@ -9,7 +9,7 @@ object AppConfig {
   val xScaleSize = scala.util.Properties.envOrElse("XSCALE_RESIZE", "1920").toInt
   val yScaleSize = scala.util.Properties.envOrElse("YSCALE_RESIZE", "1080").toInt
   val blackAndWhiteFactor = scala.util.Properties.envOrElse("BW_FACTOR", "1.2").toFloat
-  val brightenOffset = scala.util.Properties.envOrElse("BRIGHTEN_OFFSET", "1.1").toFloat
+  val brightenOffset = scala.util.Properties.envOrElse("BRIGHTEN_OFFSET", "1.2").toFloat
   val energy = scala.util.Properties.envOrElse("ENERGY", "12 GeV")
   val boundingBoxExtraSpace = scala.util.Properties.envOrElse("BOUNDING_BOX_EXTRA_SPACE", "2").toInt
   val wordDetectPixelLimit = scala.util.Properties.envOrElse("WORD_DETECT_LIMIT", "5").toInt
